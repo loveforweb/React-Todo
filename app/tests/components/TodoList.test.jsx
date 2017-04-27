2a2a9d0f8481866bebde31c7d0e45d1b612396cb
Jag Chumber <jag@loveforweb.com>
2017-04-27 12:19:58 +0100

    Added tests and updated package for testing modules

---
+++ app/tests/components/TodoList.test.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import expect from 'expect';
+import $ from 'jQuery';
+import TestUtils from 'react-dom/test-utils';
+
+import TodoList from 'TodoList';
+import Todo from 'Todo';
+
+describe('TodoList', () => {
+    it('should exist', () => {
+        expect(TodoList).toExist();
+    });
+
+    it('should render one Todo component for each todo item', () => {
+        var todos = [{
+            id: 1,
+            text: 'do something'
+        },{
+            id: 2,
+            text: 'do something else'
+        }]
+        
+        var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos} />);
+        var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+
+        expect(todosComponents.length).toBe(todos.length);
+    });
+});
