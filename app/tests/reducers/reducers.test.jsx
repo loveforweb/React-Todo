d8dec26604337357c0c054ed8b69b1a112d98a4c
Jag Chumber <jag@loveforweb.com>
2017-05-04 16:36:21 +0100

    Added todos reducers and test

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,7 +1,7 @@
 import expect from 'expect';
 import df from 'deep-freeze-strict';
 
-import { searchTextReducer, showCompletedReducer } from 'reducers';
+import { searchTextReducer, showCompletedReducer, todosReducer} from 'reducers';
 
 describe('Reducers', () => {
     describe('searchTextReducer', () => {
@@ -26,6 +26,40 @@ describe('Reducers', () => {
             var res = showCompletedReducer(df(false), df(action));
 
             expect(res).toEqual(true);
+        });
+    });
+
+    describe('todosReducer', () => {
+        it('should add new todo', () => {
+            var action = {
+                type: 'ADD_TODO',
+                text: 'Clean garden'
+            };
+
+            var res = todosReducer(df([]), df(action));
+
+            expect(res.length).toEqual(1)
+            expect(res[0].text).toEqual(action.text);
+        });
+
+        it('should toggle todo', () => {
+            var todos = [{
+                id: 123,
+                text: 'Clean garden',
+                completed: true ,
+                createdAt: 123,
+                completedAt: 125
+            }];
+
+            var action = {
+                type: 'TOGGLE_TODO',
+                id: 123
+            };
+
+            var res = todosReducer(df(todos), df(action));
+
+            expect(res[0].completed).toEqual(false)
+            expect(res[0].completedAt).toEqual(undefined);
         });
     });
 });
